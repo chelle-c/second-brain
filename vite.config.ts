adba7aa4d1d8c5b28d3101a2abfb0980cb63caff
chelle-c <michellelcroke@gmail.com>
2025-11-21 17:18:10 -0330

    Added Vite chunk splitter plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,51 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		tsconfigPaths(),
+		chunkSplitPlugin({
+			strategy: "default",
+			customSplitting: {
+				// App folders
+				"apps": [/src\/apps/],
+				"hooks": [/src\/hooks/],
+				"lib": [/src\/lib/],
+				"stores": [/src\/stores/],
+				"components": [/src\/components/],
+
+				// Vendors
+				"lucide-vendor": [/node_modules\/lucide-react/],
+				"react-vendor": [/node_modules\/react/, /node_modules\/react-dom/],
+				"tailwind-vendor": [/node_modules\/tailwindcss/],
+
+				// Yoopta
+				"yoopta-action-menu-list": [/node_modules\/@yoopta\/action-menu-list/],
+				"yoopta-blockquote": [/node_modules\/@yoopta\/blockquote/],
+				"yoopta-callout": [/node_modules\/@yoopta\/callout/],
+				"yoopta-code": [/node_modules\/@yoopta\/code/],
+				"yoopta-editor": [/node_modules\/@yoopta\/editor/],
+				"yoopta-embed": [/node_modules\/@yoopta\/embed/],
+				"yoopta-file": [/node_modules\/@yoopta\/file/],
+				"yoopta-headings": [/node_modules\/@yoopta\/headings/],
+				"yoopta-image": [/node_modules\/@yoopta\/image/],
+				"yoopta-link": [/node_modules\/@yoopta\/link/],
+				"yoopta-link-tool": [/node_modules\/@yoopta\/link-tool/],
+				"yoopta-lists": [/node_modules\/@yoopta\/lists/],
+				"yoopta-marks": [/node_modules\/@yoopta\/marks/],
+				"yoopta-paragraph": [/node_modules\/@yoopta\/paragraph/],
+				"yoopta-toolbar": [/node_modules\/@yoopta\/toolbar/],
+				"yoopta-video": [/node_modules\/@yoopta\/video/],
+			},
+		}),
+	],
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
@@ -30,4 +69,7 @@ export default defineConfig(async () => ({
 			ignored: ["**/src-tauri/**"],
 		},
 	},
+	build: {
+		chunkSizeWarningLimit: 2000,
+	}
 }));
